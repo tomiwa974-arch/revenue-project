b45b7d6d158078c19e0b735f4b0028ba2c22e54b
Tomiwa <etomiwa974@gmail.com>
2026-01-14 01:34:27 +0100

    fuck me

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -4,5 +4,5 @@ import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
   plugins: [tailwindcss(), react()],
-  base: './', // <-- add this line
+  base: '/', // <-- MUST be '/' for Vercel + React Router
 });
